d0666c5130a67c2f23fc7116dda0847670608d6d
Shubhankar <Shubhankargambhir013@gmail.com>
2022-03-28 19:11:34 +0530

    added test cases

---
+++ test/good_test/test11.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	i:=0
+	j:=0
+	for j<=50 {
+       j+=1
+	   if(j>=20){
+			for 1 {
+				i+=1
+				if(i==5){
+					i = 0
+					break;
+				}
+			}
+		} else if (j <= 15){
+			j += 3
+		} else {
+			continue
+		}
+		i -= 1;
+	}
+	
+}
+
---
+++ test/good_test/test9.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	
+	j:=0
+	for j<=50 {
+       j+=1
+	   if(j>=20){
+			i:=0
+			for i<=5 {
+				i+=1
+			}
+		} else{
+			j += 3
+		}
+
+	}
+}
+
+
